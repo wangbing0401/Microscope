8cd858b52fc2e131a50b08029d3f9a8844c3159d
wangbing <wangb@tongxinyiliao.com>
2015-11-19 14:11:26 +0800

    设置  的  helper

---
+++ client/templates/posts/post_item.js
@@ -0,0 +1,10 @@
+/**
+ * Created by tx-0020 on 15/11/19.
+ */
+Template.postItem.helpers({
+    domain: function() {
+        var a = document.createElement('a');
+        a.href = this.url;
+        return a.hostname;
+    }
+});
