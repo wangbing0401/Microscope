024330d43d14298241f90ecf24fef22675ae1b88
wangbing <wangb@tongxinyiliao.com>
2015-11-19 14:07:28 +0800

    添加了基本的 post 列表模板和静态数据

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,20 @@
+/**
+ * Created by tx-0020 on 15/11/19.
+ */
+var postsData = [
+    {
+        title: 'Introducing Telescope',
+        url: 'http://sachagreif.com/introducing-telescope/'
+    },
+    {
+        title: 'Meteor',
+        url: 'http://meteor.com'
+    },
+    {
+        title: 'The Meteor Book',
+        url: 'http://themeteorbook.com'
+    }
+];
+Template.postsList.helpers({
+    posts: postsData
+});
