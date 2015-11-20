2aa5affb4f182ff8553bf9d47b6cc5ebb5c74281
wangbing <wangb@tongxinyiliao.com>
2015-11-20 16:36:37 +0800

    添加帖子编辑表单。

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -2,6 +2,9 @@
  * Created by tx-0020 on 15/11/19.
  */
 Template.postItem.helpers({
+    ownPost: function() {
+        return this.userId === Meteor.userId();
+    },
     domain: function() {
         var a = document.createElement('a');
         a.href = this.url;
